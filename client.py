2b857c6be93aaf5f5ebe2abed8ae774705191f5a
Taft Ring <ringtaft@gmail.com>
2024-04-08 12:42:40 -0700

    imports

--- client.py
+++ client.py
@@ -1,1 +1,4 @@
 #This is the start of the Secure Client-Server Chat Application
+import sys
+import socket
+import select
---
+++ server.py
@@ -0,0 +1,9 @@
+#serverside code
+import sys
+import sys
+import select
+
+HOST = ''
+SOCKET_LIST = []
+RECV_BUFFER = 4096
+PORT = 9009
