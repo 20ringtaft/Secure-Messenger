e7369944ee4e7d192bfdabcb310aa0ab4a9b43ca
Taft Ring <ringtaft@gmail.com>
2024-04-10 12:13:07 -0700

    updated comments, git webhook test

--- client.py
+++ client.py
@@ -1,4 +1,10 @@
-#This is the start of the Secure Client-Server Chat Application
+#Clientside program 
+#Created by: Daniel Nam, Taft Ring, and Robert Tabares
+#CS 3800.01
+#April 13 2024
+
+#The purpose of this program is to provide a secure client server chat application that can queue up to 10 users. 
+
 import sys
 import socket
 import select
--- server.py
+++ server.py
@@ -1,4 +1,9 @@
-#serverside code
+#Serverside program
+#Created by: Daniel Nam, Taft Ring, and Robert Tabares
+#CS 3800.01
+#April 13 2024
+
+#The purpose of this program is to provide a secure client server chat application that can queue up to 10 users. 
 import socket
 import sys
 import select
