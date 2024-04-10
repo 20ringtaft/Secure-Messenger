9adb8d3d315cebf5527fa89b961075aa7ae5741b
Taft Ring <ringtaft@gmail.com>
2024-04-10 13:49:06 -0700

    commented serverside code

--- server.py
+++ server.py
@@ -26,7 +26,7 @@ def server():
     print ("Server initializing ... Success. Established on port " + str(PORT))
     
     while 1:
-        ready_to_read, ready_to_wrote, in_error = select.select(SOCKET_LIST, [], [], 0)
+        ready_to_read, ready_to_write, in_error = select.select(SOCKET_LIST, [], [], 0)
         
         for sock in ready_to_read: 
             #handles new connections
@@ -39,25 +39,30 @@ def server():
                 
             else:
                 try: 
+                    #receive data from the socket
                     data = sock.recv(RECV_BUFFER)
                     if data: 
+                        #check if hte socket is live
                         broadcast(server_socket, sock, "\r" + '[' + str(sock.getpeername()) + '] ' + data)
                     else: 
+                        #if not, remove the socket and announce that the user is no longer in the chat
                         if sock in SOCKET_LIST:
                             SOCKET_LIST.remove(sock)
                             
                         broadcast(server_socket, sock, "Client (%s, %s) is offline\n" % addr)
                         continue
                 except: 
+                    #if a socket cannot connect, notify connected users that other user cannot connect
                     broadcast(server_socket, sock, "Client (%s,%s) is offline\n" % addr)
                     continue
     server_socket.close()  
+#send message to connected users
 def broadcast (server_socket, sock, message):
     for socket in SOCKET_LIST: 
         if socket != server_socket and socket != sock:
             try:
                 socket.send(message)
-            except: 
+            except: #if socket is bad, close socket and remove it from the list 
                 socket.close
                 if socket in SOCKET_LIST: 
                     SOCKET_LIST.remove(socket)
