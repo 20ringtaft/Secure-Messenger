f13901643ddf784599f21d787f1907df79dce76d
Daniel Dan <daniel.daehan.nam@gmail.com>
2024-04-12 22:13:18 -0700

    Fixed bug

--- client.py
+++ client.py
@@ -1,17 +1,10 @@
-#Clientside program 
-#Created by: Daniel Nam, Taft Ring, and Robert Tabares
-#CS 3800.01
-#April 13 2024
-
-#The purpose of this program is to provide a secure client server chat application that can queue up to 10 users. 
-
 import sys
 import socket
 import select
  
 def client():
     if(len(sys.argv) < 3) :
-        print ('Usage : python chat_client.py hostname port')
+        print('Usage : python chat_client.py hostname port')
         sys.exit()
 
     host = sys.argv[1]
@@ -20,35 +13,37 @@ def client():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(2)
      
-    try :
+    try:
         s.connect((host, port))
-    except :
-        print ('Unable to connect')
+    except:
+        print('Unable to connect')
         sys.exit()
      
-    print ('Connected to remote host. You can start sending messages')
-    sys.stdout.write('[Me] '); sys.stdout.flush()
+    print('Connected to remote host. You can start sending messages')
+    print('[Me] ', end='', flush=True)
      
-    while 1:
-        socket_list = [sys.stdin, s]
+    while True:
+        socket_list = [s]
          
-        ready_to_read,ready_to_write,in_error = select.select(socket_list , [], [])
+        ready_to_read, _, _ = select.select(socket_list, [], [])
          
         for sock in ready_to_read:             
             if sock == s:
                 data = sock.recv(4096)
-                if not data :
-                    print ('\nDisconnected from chat server')
+                if not data:
+                    print('\nDisconnected from chat server')
                     sys.exit()
-                else :
-                    sys.stdout.write(data)
-                    sys.stdout.write('[Me] '); sys.stdout.flush()     
+                else:
+                    print(data.decode(), end='', flush=True)
+                    print('[Me] ', end='', flush=True)
             
-            else :
-                msg = sys.stdin.readline()
-                s.send(msg)
-                sys.stdout.write('[Me] '); sys.stdout.flush() 
+        try:
+            msg = sys.stdin.readline()
+            s.send(msg.encode())
+        except KeyboardInterrupt:
+            print("\nClosing connection...")
+            s.close()
+            sys.exit()
 
 if __name__ == "__main__":
-
-    sys.exit(client())
+    client()
